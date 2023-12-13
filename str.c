72881f3aaf9734c312b273c29fa3c2dbd844f5b6
amin ahmed <aminahmed24@gmail.com>
2023-12-13 19:50:58 +0200

    fix _strcmp

--- str.c
+++ str.c
@@ -16,14 +16,10 @@ int _strcmp(const char *str1, const char *str2)
 {
 	int i = 0;
 
-	while (str1[i])
+	while (str1[i] && str2[i])
 	{
-		if (str1[i] > str2[i])
-			return (1);
-		else if (str1[i] < str2[i])
-			return (-1);
-		if (str1[i] != 0 || str2[i] != 0)
-			return (0);
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
 		i++;
 	}
 	return (str1[i] - str2[i]);
